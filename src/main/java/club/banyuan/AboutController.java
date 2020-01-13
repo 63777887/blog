07820455b837332175c66fc13e730c502685806f
63777887@qq.com <63777887@qq.com>
2020-01-13 10:57:40 +0800

    init blog repo

---
+++ src/main/java/club/banyuan/AboutController.java
@@ -0,0 +1,14 @@
+package club.banyuan;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AboutController {
+    @GetMapping("/about")
+    String getAbout(){
+        return "about";
+    }
+}
