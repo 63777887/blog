24f59bf84ad2718bd34a9462c467a2eddacd9439
63777887@qq.com <63777887@qq.com>
2020-01-13 13:05:39 +0800

    add

--- src/main/java/club/banyuan/IndexController.java
+++ src/main/java/club/banyuan/IndexController.java
@@ -1,5 +1,17 @@
 package club.banyuan;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class IndexController {
-    
+
+    @GetMapping("/user/{username:[a-z0-9_]+}")
+    @ResponseBody
+    String getIndex(@PathVariable String username){
+//        return String.format("user %s",username);
+        return "user: "+username;
+    }
 }
